66462721f82626bf1691bbd453997a40b4edd20e
Shane Avery Sistoza <me@ssistoza.dev>
2021-06-20 12:29:26 -0400

    Add new electric schema

---
+++ prisma/migrations/20210620145356_added_electric_feat/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "ElectricCompany" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(25) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ElectricBill" (
+    "id" SERIAL NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "companyId" INTEGER NOT NULL,
+    "usage" INTEGER NOT NULL,
+    "cost" MONEY NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ElectricBill" ADD FOREIGN KEY ("companyId") REFERENCES "ElectricCompany"("id") ON DELETE CASCADE ON UPDATE CASCADE;
